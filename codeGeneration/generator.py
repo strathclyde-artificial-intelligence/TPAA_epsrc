fc932e1b174d15ac7355d804d949ef0d2e97934e
Tim Enos CS2019 <tim.enos.2020@uni.strath.ac.uk>
2022-07-01 13:06:59 +0200

    first iteration of fully generated statements

--- codeGeneration/generator.py
+++ codeGeneration/generator.py
@@ -42,21 +42,6 @@ operation_ret = {
         "operation_ret1": "return {1}", 
         }
 
-''' First, we set either an operation node or a condition node as the head node, which is the first operation to be performed
-
-    Then, we attach either an operation node or a condition node to an available slot in the structure randomly.
-
-    We continue attaching new nodes until the structure contains exactly c nodes.
-
-    Finally, we attach return nodes to all the remaining slots. Figure 7 shows some generated structures of varying complexity.
-
-'''
-
-'''
-    graph.statement
-    graph.connections = A -> key, B -> key
-
-'''
 
 class ProgrammingGenerator:
     def __init__(self):
@@ -66,6 +51,7 @@ class ProgrammingGenerator:
         self.actions = ["A", "B"]
         self.operands = ["{1}", "{2}"]
         self.output = "{O}"
+        self.keywords = ["return {1}", "If", "Get"]
 
 
     def start(self, complexity):
@@ -98,51 +84,149 @@ class ProgrammingGenerator:
                 self.statements[key] = "return {1}"
                 self.graph[key] = None
 
-        self.assign_node_parameters()
+        self.assign_node_parameters(complexity)
 
 
-    def assign_node_parameters(self):
+    def assign_node_parameters(self, complexity):
         graph_list_keys = list(self.graph.keys())
         #We define a critical node as a node whose operation affects the outcome of the function. Initially, we add all return nodes and condition nodes to critical_nodes
-        critical_nodes = []
+        critical_nodes = set()
 
         #start by adding all the nodes which are critical to critical_nodes
         for index in graph_list_keys:
-            if "return {1}" in self.statements[index] or "If" in self.statements[index]:
-                critical_nodes.append(index)
+            if self.keywords[0] in self.statements[index] or self.keywords[1] in self.statements[index]:
+                critical_nodes.add(index)
 
         x_var = "x"
         count = 1
-
         stack = []
-        #might have to change how the loop works just now
+
+        #this needs to be refactored into more functions, rather then being a huge cluster....
         for index in graph_list_keys:
             if index not in critical_nodes:
                 statement = self.statements[index]
-                operand_to_replace = random.choice(self.operands)
                 new_statement = statement.replace(self.output, x_var+str(count))
                 self.statements[index] = new_statement
+                stack.append(x_var+str(count))
 
-                random_node = random.choice(critical_nodes)
+                visited = set()
+                #get the path that the index is traversing
+                visited = self.dfs(visited, index)
 
-                if self.operands[0] in self.statements[random_node] or self.operands[1] in self.statements[random_node]:
-                    visited = set()
-                    #here we check if it is reachable
-                    visited = self.dfs(visited, index)
-                    if random_node in visited:
-                        statement = self.statements[random_node]
-                        operand_to_replace = random.choice(self.operands)
+                #we loop through all of the visited node from index, then we check for all conditional statements that follows node
+                list_of_statements = []
+                for node in visited:
+                    if node in critical_nodes and self.keywords[1] in self.statements[node]:
+                        list_of_statements.append(node)
+
+                #if there follows more then 0 If nodes from index, then we pick a random one and assign X_num to one of its operands
+                if len(list_of_statements) > 0:
+                    node = random.choice(list_of_statements)
+                    operand_to_replace = random.choice(self.operands)
+                    statement = self.statements[node]
+                    if self.operands[0] in self.statements[index] and self.operands[1] in self.statements[index]:
                         if operand_to_replace in statement:
-                            new_statement = statement.replace(operand_to_replace, x_var+str(count))
-                            self.statements[random_node] = new_statement
+                            new_statement = statement.replace(self.operands[0], x_var +str(count))
+                            self.statements[node] = new_statement
                         #if we the other slot is the free one
                         elif operand_to_replace == self.operands[0]:
-                            new_statement = statement.replace(self.operands[1], x_var+str(count))
-                            self.statements[random_node] = new_statement
+                            new_statement = statement.replace(self.operands[0], x_var +str(count))
+                            self.statements[node] = new_statement
                         #if we the other slot is the free one
                         elif operand_to_replace == self.operands[1]:
-                            new_statement = statement.replace(self.operands[0], x_var+str(count))
-                            self.statements[random_node] = new_statement
+                            new_statement = statement.replace(self.operands[1], x_var +str(count))
+                            self.statements[node] = new_statement
+                else:
+                    #we check if there is more then 1 node that is folling, and if this nodes is an operation node if this is the case then we assign one of the variables to this
+                    next_node = (list(visited)[1])
+                    if self.keywords[2] in self.statements[next_node]:
+                        operand_to_replace = random.choice(self.operands)
+                        statement = self.statements[next_node]
+                        if self.operands[0] in self.statements[next_node] and self.operands[1] in self.statements[next_node]:
+                            if operand_to_replace in statement:
+                                new_statement = statement.replace(self.operands[0], x_var +str(count))
+                                self.statements[next_node] = new_statement
+                            #if the other slot was the random one then we assign this to the 
+                            elif operand_to_replace == self.operands[0]:
+                                new_statement = statement.replace(self.operands[0], x_var +str(count))
+                                self.statements[next_node] = new_statement
+                            #if we the other slot is the free one
+                            elif operand_to_replace == self.operands[1]:
+                                new_statement = statement.replace(self.operands[1], x_var +str(count))
+                                self.statements[next_node] = new_statement
+                    #if the following statements is a return statement then we can return the variable we just stored
+                    elif self.keywords[0] in self.statements[next_node]:
+                        statement = self.statements[next_node]
+                        new_statement = statement.replace(self.operands[0], x_var +str(count))
+                        self.statements[next_node] = new_statement
+                count+=1
+            else:
+                #we check if it is a conditional statement, if it is we add a random number to one of the operands
+                if self.keywords[1] in self.statements[index]:
+                    rand_number = random.randint(0,100)
+                    operand_to_replace = random.choice(self.operands)
+                    statement = self.statements[index]
+                    if self.operands[0] in self.statements[index] and self.operands[1] in self.statements[index]:
+                        if operand_to_replace in statement:
+                            new_statement = statement.replace(operand_to_replace, str(rand_number))
+                            self.statements[index] = new_statement
+                        #if we the other slot is the free one
+                        elif operand_to_replace == self.operands[0]:
+                            new_statement = statement.replace(self.operands[1], str(rand_number))
+                            self.statements[index] = new_statement
+                        #if we the other slot is the free one
+                        elif operand_to_replace == self.operands[1]:
+                            new_statement = statement.replace(self.operands[0], str(rand_number))
+                            self.statements[index] = new_statement
+
+
+        #get the statements that have 2 wholes to fill up and assign at least one random int to these, no matter if it is a critical node or not
+
+        self.add_function_input(complexity)
+        self.fill_remaining()
+
+        print(stack)
+        print(critical_nodes)
+        print(self.statements)
+        print(self.graph)
+
+
+    #this function adds input parameters into the statement
+    def add_function_input(self, complexity):
+        y_var = "y"
+        #we create the number of input nodes, we do this by creating complexity - 1 input nodes, 
+        inputs_to_add = []
+        for i in range(1, complexity):
+            inputs_to_add.append(y_var+str(i))
+
+        graph_list_keys = list(self.graph.keys())
+        #we pick any random node from the graph and check if it has some free slot of any sort
+        random_node = random.choice(graph_list_keys)
+        operand_to_replace = random.choice(self.operands)
+
+        max_tries = 100
+        count = 0
+
+        while count < len(inputs_to_add):
+            random_node = random.choice(graph_list_keys)
+            operand_to_replace = random.choice(self.operands)
+            if operand_to_replace in self.statements[random_node]:
+                new_statement = self.statements[random_node].replace(operand_to_replace, inputs_to_add[count])
+                self.statements[random_node] = new_statement
+                count+=1
+            
+    def fill_remaining(self):
+        graph_list_keys = list(self.graph.keys())
+
+        for index in graph_list_keys:
+            if self.operands[0] in self.statements[index]:
+                rand_number = str(random.randint(0, 100))
+                new_statement = self.statements[index].replace(self.operands[0], rand_number)
+                self.statements[index] = new_statement
+            if self.operands[1] in self.statements[index]:
+                rand_number = str(random.randint(0, 100))
+                new_statement = self.statements[index].replace(self.operands[1], rand_number)
+                self.statements[index] = new_statement
 
     
     def dfs(self, visited, node):  #function for dfs 
@@ -154,8 +238,6 @@ class ProgrammingGenerator:
                 for neighbour in self.graph[node]:
                     self.dfs(visited, neighbour)
         return visited
-
-
 
 
 
